1ceb1bc823edfba8f6f94835d6ec0a307a762fca
okwir <francisokwir24@gmail.com>
2019-03-12 12:15:44 +0300

    initial commit

---
+++ billing.js
@@ -0,0 +1,4 @@
+var transactionCharge= 0.025; //this is the percentage that determines the transaction charge
+var amountTendered= 100000;
+console.log ('This is what the billed amount is (less transaction charges);');
+console.log(amountTendered-(amountTendered*transactionCharge));
